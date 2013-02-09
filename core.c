d26f5e924d927ac866c349ea181e29875e4b9b20
Chris A. Taylor <chris@gameclosure.com>
2013-02-08 16:22:16 -0800

    Fix context restore

--- core.c
+++ core.c
@@ -264,9 +264,11 @@ void core_tick(int dt) {
 				context_2d_translate(ctx, tex_size.height/2.f/tex->scale, tex_size.width/2.f/tex->scale);
 				context_2d_rotate(ctx, 3.14f/2.f);
 				context_2d_translate(ctx, -tex_size.width/2.f/tex->scale, -tex_size.height/2.f/tex->scale);
-				context_2d_restore(ctx);
 			}
 			context_2d_drawImage(ctx, 0, splash, &tex_size, &size, 0);
+			if (rotate) {
+				context_2d_restore(ctx);
+			}
 			// we're the first, last, and only thing to draw, so flush the buffer
 			context_2d_flush(ctx);
 
