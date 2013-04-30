d5d92ee45cfdfb138d3b0df833132693a58ea8fb
Mario Balibrera <mario@gameclosure.com>
2013-04-30 13:17:32 -0700

    seek_to added to sound manager header file

--- platform/sound_manager.h
+++ platform/sound_manager.h
@@ -27,6 +27,7 @@ void sound_manager_play_sound(const char *url, float volume, bool loop);
 void sound_manager_stop_sound(const char *url);
 void sound_manager_pause_sound(const char *url);
 void sound_manager_set_volume(const char *url, float volume);
+void sound_manager_seek_to(const char *url, float position);
 void sound_manager_load_background_music(const char *url);
 void sound_manager_play_background_music(const char *url, float volume, bool loop);
 void sound_manager_stop_loading_sound();
