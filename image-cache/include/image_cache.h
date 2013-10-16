90206795a842d508463ec9771414747dd66e4529
DerekBurch <derek@gameclosure.com>
2013-10-16 11:26:26 -0700

    added extern c to image cache header

--- image-cache/include/image_cache.h
+++ image-cache/include/image_cache.h
@@ -19,10 +19,17 @@ struct etag_data {
 // The callback provided to this function is when a url provided to image_cache_load has finished loading
 // It is called as soon as possible if the image is cached on disk and called a second time if the cache 
 // discovers a newer image on the server.
+#if __cplusplus
+extern "C" {
+#endif
+
 void image_cache_init(const char *path, void (*load_callback)(struct image_data*));
 void image_cache_destroy();
 struct image_data *image_cache_get_image(const char *url);
 void image_cache_remove(const char *url);
 void image_cache_load(const char *url);
 
+#if __cplusplus
+} //extern C
+#endif
 #endif //IMAGE_CACHE_H_
